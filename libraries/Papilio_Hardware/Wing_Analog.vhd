3f08dd1a150ba59e5bae65ded1f68efe85ebb562
jackgassett <jack.gassett@gadgetfactory.net>
2014-12-02 10:45:22 -0700

    Fix Wing_Analog symbol

--- libraries/Papilio_Hardware/Wing_Analog.vhd
+++ libraries/Papilio_Hardware/Wing_Analog.vhd
@@ -20,9 +20,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-library zpuino;
-use zpuino.pad.all;
-
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
