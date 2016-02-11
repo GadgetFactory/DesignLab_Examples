67b7cf55feb86843530cd7440def40db29f17da0
Jack Gassett <jack.gassett@gadgetfactory.net>
2016-02-10 17:24:59 -0700

    Fix for bad handling of channel groups and sample size.

--- libraries/Wishbone_Sump_LA/examples/OLS_Client/OLS_Client.ino
+++ libraries/Wishbone_Sump_LA/examples/OLS_Client/OLS_Client.ino
@@ -13,7 +13,7 @@ private:
     
 public:
     uint32_t *buffer;
-    size_t samples;
+    uint32_t samples;
 
     Capture(): BaseDevice(-1) {
     }
@@ -23,32 +23,50 @@ public:
         } else {
             printf("Cannot find device\n");
         }
-        samples = 8192;
+        //samples = 300000;
+        samples = 65536;
         buffer = (uint32_t*)malloc(samples*sizeof(uint32_t));
+        //memset(buffer, 0xff, samples*sizeof(uint32_t));
         armed=0;
-        channel0=1;
-        channel1=1;
-        channel2=1;
-        channel3=1;
+        channel0=0;
+        channel1=0;
+        channel2=0;
+        channel3=0;
     }
     unsigned getStatus()
     {
         return REG(0);
     }
+    
+    unsigned getTriggerAddress()
+    {
+        return REG(1);
+    }    
+    
+    unsigned getSamples()
+    {
+        return samples;
+    }       
+    
     void longCommand(uint8_t opcode, uint32_t data)
     {
+        uint32_t tmpData;
         switch (opcode) {
         case 0x81: // Size
             //samples = (data&0xffff)+((data>>16)&0xffff);
-            samples = ((((data>>16)&0x00ff)*0x100 + ((data>>24)&0xff))+1)*4;
+            //samples = ((((data>>16)&0x00ff)*0x100 + ((data>>24)&0xff)));
+            tmpData = (data>>16);  //Shift Read Count over
+            samples = ((tmpData & 0xFFU) << 8 | (tmpData & 0xFF00U) >> 8)*4 + 4; //Reverse bytes of Read Count
             //samples = (((data>>16)&0x00ff)*0x100 + ((data>>24)&0xff)) + (((data&0xff)*0x100) + ((data>>8)&0xff));
             
-            free(buffer);
-            buffer = (uint32_t*)malloc(samples*sizeof(uint32_t));            
-//            buffer = (uint32_t*) realloc(buffer,samples);
-            REG(100) = (unsigned)&buffer[0];
-            REG(101) = (unsigned)&buffer[samples];
-            //Serial.println(samples, HEX);
+            //free(buffer);
+            //buffer = (uint32_t*)malloc(samples*sizeof(uint32_t));            
+
+            //buffer = (uint32_t*) realloc(buffer,samples);
+            //memset(buffer, 0xff, samples*sizeof(uint32_t));            
+            //REG(100) = (unsigned)&buffer[0];
+            //REG(101) = (unsigned)&buffer[samples];
+            //Serial.write(samples);
             break;
         case 0x82: //Channel Groups
             channel0 = data>>26&0x01;
@@ -71,19 +89,21 @@ public:
     
     void start()
     {
-        REG(100) = (unsigned)&buffer[0];
-        REG(101) = (unsigned)&buffer[samples];
+        //REG(100) = (unsigned)&buffer[0];
+        //REG(101) = (unsigned)&buffer[samples];
         
   //Set base address for shared memory
-  //REGISTER(IO_SLOT(14),0x100) = (unsigned)&buffer[0];
+  REGISTER(IO_SLOT(14),0x100) = (unsigned)&buffer[0];
   //Set end of shared memory
   //REGISTER(IO_SLOT(14),0x101) = (unsigned)&buffer[8192];         
+  REGISTER(IO_SLOT(14),0x101) = (unsigned)&buffer[samples];         
 
     }
     void reset()
     {
         //REG(2) = 2;
         REG(0) = 0;
+        armed = 0;
     }
     void arm()
     {
@@ -100,16 +120,21 @@ public:
     void sendIfReady()
     {
         int i;
+        unsigned trigAddress = 0;
         if (armed) {
             if ((getStatus())==0x43) {
                 // Transmit.
                 //Serial.println("In Transmit");
+                trigAddress = getTriggerAddress();
+                //Serial.print("Trigger address: ");
+                //Serial.println(trigAddress);
+                //for (i=samples+trigAddress;i>=trigAddress; i--) {
                 for (i=samples;i>=0; i--) {
                 //for (i=0;i<=samples; i++) {
-                  if (channel0=1) Serial.write(buffer[i]&0xff);
-                  if (channel1=1) Serial.write((buffer[i]>>8)&0xff);
-                  if (channel2=1) Serial.write((buffer[i]>>16)&0xff);
-                  if (channel3=1) Serial.write((buffer[i]>>24)&0xff);                  
+                  if (channel0==0) Serial.write(buffer[i]&0xff);
+                  if (channel1==0) Serial.write((buffer[i]>>8)&0xff);
+                  if (channel2==0) Serial.write((buffer[i]>>16)&0xff);
+                  if (channel3==0) Serial.write((buffer[i]>>24)&0xff);                  
                 }
               reset();
             }
@@ -177,13 +202,20 @@ void handleSerial(uint8_t v)
             //Serial.print("1ALS");
             break;
         case 0x61: // getStatus
-            Serial.println(mycap.getStatus(), HEX);
+            Serial.println(mycap.getStatus());
             break;  
         case 0x62: // getStatus
             for (int i = 0; i < serialIndex; i++ ){
              Serial.write(serialBuffer[i]); 
             }      
-            break;    
+            break;   
+        case 0x63: // getTrigger
+
+             Serial.write(mycap.getTriggerAddress());   
+            break; 
+        case 0x64: // getSamples
+             Serial.println(mycap.getSamples(), HEX);   
+            break;              
         default:
             if (v & 0x80) {
                 //longcommand = v;
@@ -226,8 +258,8 @@ void loop()
     {
         int i = Serial.read();
         //Serial.println(i, HEX);
-        serialBuffer[serialIndex] = i;
-        serialIndex++;
+//        serialBuffer[serialIndex] = i;
+//        serialIndex++;
         handleSerial(i&0xff);
         //handleSerial(i);
     }
