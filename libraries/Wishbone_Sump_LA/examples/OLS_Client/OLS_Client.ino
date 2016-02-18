fcdc6a34ce307520319a21d7fdeb9d2a8188178e
Jack Gassett <jack.gassett@gadgetfactory.net>
2016-02-18 16:09:12 -0700

    Cleanup.

--- libraries/Wishbone_Sump_LA/examples/OLS_Client/OLS_Client.ino
+++ libraries/Wishbone_Sump_LA/examples/OLS_Client/OLS_Client.ino
@@ -25,7 +25,6 @@ public:
         } else {
             printf("Cannot find device\n");
         }
-        //samples = 300000;
         samples = 300000;
         //buffer = (uint32_t*)malloc(samples*sizeof(uint32_t));
         //memset(buffer, 0xff, samples*sizeof(uint32_t));
@@ -37,17 +36,6 @@ public:
     }
     unsigned getStatus()
     {
-//        int status = REG(0);
-//        Serial.print("Status: ");
-//        Serial.println(status, BIN);
-//        Serial.println(status, HEX); 
-//        Serial.print("triggered: ");
-//        Serial.println(status>>1&0x01, HEX);             
-//        Serial.print("send: ");
-//        Serial.println(status>>8&0x01, HEX);   
-//        Serial.print("Fifo Empty: ");
-//        Serial.println(status>>6&0x01, HEX);  
-
         return REG(0);        
     }
     
@@ -67,34 +55,14 @@ public:
         uint32_t tmpData;
         switch (opcode) {
         case 0x81: // Size
-            //samples = (data&0xffff)+((data>>16)&0xffff);
-            //samples = ((((data>>16)&0x00ff)*0x100 + ((data>>24)&0xff)));
             tmpData = (data>>16);  //Shift Read Count over
             samples = ((tmpData & 0xFFU) << 8 | (tmpData & 0xFF00U) >> 8)*4 + 4; //Reverse bytes of Read Count
-            //samples = (((data>>16)&0x00ff)*0x100 + ((data>>24)&0xff)) + (((data&0xff)*0x100) + ((data>>8)&0xff));
-            
-            //free(buffer);
-            //buffer = (uint32_t*)malloc(samples*sizeof(uint32_t));            
-
-            //buffer = (uint32_t*) realloc(buffer,samples);
-            //memset(buffer, 0xff, samples*sizeof(uint32_t));            
-            //REG(100) = (unsigned)&buffer[0];
-            //REG(101) = (unsigned)&buffer[samples];
-            //Serial.write(samples);
             break;
         case 0x82: //Channel Groups
             channel0 = data>>26&0x01;
             channel1 = data>>27&0x01;
             channel2 = data>>28&0x01;
             channel3 = data>>29&0x01;
-//            Serial.print("Channel0: ");
-//            Serial.println(channel0);
-//            Serial.print("Channel1: ");
-//            Serial.println(channel1);
-//            Serial.print("Channel2: ");
-//            Serial.println(channel2);
-//            Serial.print("Channel3: ");
-//            Serial.println(channel3);
             break;
         }      
       
@@ -115,20 +83,15 @@ public:
     }
     void reset()
     {
-        //REG(2) = 2;
-        //REG(102) = 0x03;
-        //Serial.println("In Reset");
+        //REG(0) = 0;
         REGISTER(IO_SLOT(14),0) = 0;
-        //REGISTER(IO_SLOT(14),102) = 0x03;
         armed = 0;
     }
     void arm()
     {
         //REG(1) = 0;
         REGISTER(IO_SLOT(14),1) = 0;
-        armed=1;
-//        Serial.print("Armed Trigger address: ");
-//        Serial.println(getEndAddress(),HEX);        
+        armed=1;      
     }
     void abort()
     {
@@ -155,41 +118,8 @@ public:
             samplesSent = status>>3&0x01;
             memoryFilled = status>>4&0x01;
             memoryFlushed = status>>5&0x01;
-                         
-            
-            
-//            triggered = status>>1&0x01;
-//            fifoEmpty = status>>6&0x01;
-//            memoryIdle = status&0x01;           
-//            send = status>>8&0x01;
-//            Serial.print("Status: ");
-//            Serial.println(status, BIN);
-//            Serial.println(status, HEX); 
-//            Serial.print("triggered: ");
-//            Serial.println(triggered, HEX);             
-//            Serial.print("send: ");
-//            Serial.println(send, HEX);   
-//            Serial.print("Fifo Empty: ");
-//            Serial.println(fifoEmpty, HEX);             
-            //if (fifoEmpty && triggered && memoryIdle) {
               if (status==0x1F) {
-              //if (0) {                
-                // Transmit.
-                //Serial.println("In Transmit");
-                //endAddress = getEndAddress();
-//                Serial.print("Last address: ");
-//                Serial.println(getEndAddress(), DEC);
-//                Serial.print("Base address: ");
-//                Serial.println((unsigned)&buffer[0], DEC); 
-//                Serial.print("End address: ");
-//                Serial.println((unsigned)&buffer[65536] , DEC); 
-               
-                //for (i=samples+trigAddress;i>=trigAddress; i--) {
-                //for (i=0;i<=samples; i++) {
-                //for (i=samples;i>=0; i--) {
-                uint32_t index = (getEndAddress() - (uint32_t)&buffer[0])/4;
-//                Serial.print("Index: ");
-//                Serial.println(index, DEC);                
+                uint32_t index = (getEndAddress() - (uint32_t)&buffer[0])/4;           
                 for (i=index;i>=index-samples; i--) {                  
                   if (channel0==0) Serial.write(buffer[i]&0xff);
                   if (channel1==0) Serial.write((buffer[i]>>8)&0xff);
@@ -232,23 +162,13 @@ int serialIndex = 0;
 
 void processLong(uint32_t v)
 {
-
   mycap.longCommand(opcode,longcommand);
-
-//   Serial.println(); 
-//   Serial.print("Opcode: ");
-//   Serial.println(opcode, HEX);
-//   Serial.print("LongCommand: ");
-//   Serial.println(longcommand, HEX);
-
 }
 
 void handleSerial(uint8_t v)
 {
-    //Serial.println(state, HEX);
     switch(state) {
     case IDLE:
-        //Serial.println("In Idle");
         switch (v) {
         case 0x00: // Reset
             mycap.reset();
@@ -258,7 +178,6 @@ void handleSerial(uint8_t v)
             break;
         case 0x02: // id
             Serial.write((const uint8_t*)"1ALS",4);
-            //Serial.print("1ALS");
             break;
         case 0x05: // Abort
             mycap.abort();
@@ -279,7 +198,6 @@ void handleSerial(uint8_t v)
             break;              
         default:
             if (v & 0x80) {
-                //longcommand = v;
                 opcode = v;
                 state = LONG1;
             }
@@ -287,9 +205,6 @@ void handleSerial(uint8_t v)
         }
         break;
     case LONG1:
-        //Serial.println("In LONG1");
-//        longcommand<<=8;
-//        longcommand += v;
         longcommand = v;
         state = LONG2;
         break;
@@ -318,11 +233,10 @@ void loop()
     if (Serial.available())
     {
         int i = Serial.read();
-        //Serial.println(i, HEX);
-        serialBuffer[serialIndex] = i;
-        serialIndex++;
+//        serialBuffer[serialIndex] = i;
+//        serialIndex++;
         handleSerial(i&0xff);
-        //handleSerial(i);
+
     }
     mycap.sendIfReady();
     //mycap.arm();
